453e965de59b6df736da404239b02e280f9f55a8
davlloyd <davidlloyd0@gmail.com>
2018-10-14 23:39:48 +1100

    update python slack scripts

--- functions/slack-post-deploy-vm/slack_vm_being_deployed.py
+++ functions/slack-post-deploy-vm/slack_vm_being_deployed.py
@@ -17,8 +17,4 @@ def handle(ctx, payload):
         webhook_url, data=json.dumps(slack_data),
         headers={'Content-Type': 'application/json'}
     )
-    if response.status_code != 200:
-        raise ValueError(
-            'Request to slack returned an error %s, the response is:\n%s'
-            % (response.status_code, response.text)
-        )
+    return {"status": response.status_code}
