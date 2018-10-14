b549234045836078698c26f84e37ba73035d8cc0
davlloyd <davidlloyd0@gmail.com>
2018-10-14 23:15:40 +1100

    Updated python image

--- functions/slack-post-deploy-vm/slack_vm_being_deployed.py
+++ functions/slack-post-deploy-vm/slack_vm_being_deployed.py
@@ -6,12 +6,19 @@ import requests
 webhook_url = 'https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX'
 slack_data = {'text': "Sup! We're hacking shit together @HackSussex :spaghetti:"}
 
-response = requests.post(
-    webhook_url, data=json.dumps(slack_data),
-    headers={'Content-Type': 'application/json'}
-)
-if response.status_code != 200:
-    raise ValueError(
-        'Request to slack returned an error %s, the response is:\n%s'
-        % (response.status_code, response.text)
+def handle(ctx, payload):
+    secrets = ctx["secrets"]
+    if secrets is None:
+        raise Exception("Requires slack secret")
+    webhook_url = secrets["slack_url"]
+    slack_data = {'text': "Incoming from Dispatch"}
+
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
     )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
