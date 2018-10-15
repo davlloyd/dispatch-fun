edcde46fdd08dd699f3dc9db8f81f80e3472a70a
davlloyd <davidlloyd0@gmail.com>
2018-10-15 17:42:46 +1100

    git update

--- functions/git-vmrequest/git-vmrequest.py
+++ functions/git-vmrequest/git-vmrequest.py
@@ -4,14 +4,13 @@ import requests
 def handle(ctx, payload):
     secrets = ctx["secrets"]
     if secrets is None:
-        raise Exception("Requires vcenter secret")
+        raise Exception("Requires git secret")
     clone_url = secrets["vmcloneurl"]
     host = secrets["vcenterhost"]
-    name = payload.get["name"]
+    name = payload.get["hook_id"]
     template = secrets["template"]
 
     clone_data = {'host': host, 'name': name, 'template': template}
-  
 
     response = requests.post(
         clone_url, data=json.dumps(clone_data),
