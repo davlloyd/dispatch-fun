d5ecd0a0e27b87f87ed8712b7f9746923c8911a2
davlloyd <davidlloyd0@gmail.com>
2018-10-16 08:56:26 +1100

    update git request to exclude templates

--- functions/git-vmrequest/git-vmrequest.py
+++ functions/git-vmrequest/git-vmrequest.py
@@ -16,43 +16,44 @@ def handle(ctx, payload):
     vmlist = ""
     if (len(filerequests)):
         for filename in filerequests:
-            file_url = 'https://raw.githubusercontent.com/{0}/master/{1}'.format(repopath, filename)
-            response = requests.get(file_url)
-            if response.status_code == 200:
-                entry = response.json()
+            if "template" not in filename.lower():
+                file_url = 'https://raw.githubusercontent.com/{0}/master/{1}'.format(repopath, filename)
+                response = requests.get(file_url)
+                if response.status_code == 200:
+                    entry = response.json()
 
-                name = entry["name"]
-                if(entry["template"] != ""):
-                    sourcetemplate = entry["template"]
+                    name = entry["name"]
+                    if(entry["template"] != ""):
+                        sourcetemplate = entry["template"]
+                    else:
+                        sourcetemplate = template
+                    if(entry["targethost"] != ""):
+                        targethost = entry["targethost"]
+                    else:
+                        targethost = host
+                    targetdc = entry["dc"]
+                    targetfolder = entry["vmfolder"]
+                    respool = entry["resourcepool"]
+                    poweron = entry["poweron"]                
+
+                    clone_data = {
+                        'host': targethost, 
+                        'name': name, 
+                        'template': sourcetemplate,
+                        'datacenterName': targetdc,
+                        'vmFolder': targetfolder,
+                        'resourcePool': respool,
+                        'powerOn': poweron}
+
+                    response = requests.post(
+                        clone_url, 
+                        data=json.dumps(clone_data),
+                        headers={'Content-Type': 'application/json'},
+                        verify=False
+                    )
+                    vmlist += "{0}/n".format(clone_data)
                 else:
-                    sourcetemplate = template
-                if(entry["targethost"] != ""):
-                    targethost = entry["targethost"]
-                else:
-                    targethost = host
-                targetdc = entry["dc"]
-                targetfolder = entry["vmfolder"]
-                respool = entry["resourcepool"]
-                poweron = entry["poweron"]                
-
-                clone_data = {
-                    'host': targethost, 
-                    'name': name, 
-                    'template': sourcetemplate,
-                    'datacenterName': targetdc,
-                    'vmFolder': targetfolder,
-                    'resourcePool': respool,
-                    'powerOn': poweron}
-
-                response = requests.post(
-                    clone_url, 
-                    data=json.dumps(clone_data),
-                    headers={'Content-Type': 'application/json'},
-                    verify=False
-                )
-                vmlist += "{0}/n".format(clone_data)
-            else:
-                print(file_url)
+                    print(file_url)
     else:
         return {"status": "no new requests"}
 
