a06d68fa29d2ce4b6ab381d8e0bb188ab6192023
davlloyd <davidlloyd0@gmail.com>
2018-10-17 23:16:13 +1100

    hello.py demo script

---
+++ functions/hello-py/hello-py.py
@@ -0,0 +1,8 @@
+def handle(ctx, payload):
+    name = "Noone"
+    place = "Nowhere"
+    if payload:
+        name = payload.get("name", name)
+        place = payload.get("place", place)
+    return {"myField": "Hello, %s from %s" % (name, place)}
+
